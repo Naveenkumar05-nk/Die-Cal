91490bc3dc5d50c31c53b0d2cec2797f0fc8065a
Naveenkumar05-nk <jangidnaveen05@gmail.com>
2020-10-06 17:10:03 +0530

    Updates

---
+++ precache-manifest.06b96e65765c0795419da59de173d32e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4902e3e590bd9223779b37d74e8f79",
+    "url": "/Die-Cal/index.html"
+  },
+  {
+    "revision": "525c0c394bf7fb2dc0fb",
+    "url": "/Die-Cal/static/css/main.670431ca.chunk.css"
+  },
+  {
+    "revision": "7e68185431ebe3504f25",
+    "url": "/Die-Cal/static/js/2.224214a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Die-Cal/static/js/2.224214a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "525c0c394bf7fb2dc0fb",
+    "url": "/Die-Cal/static/js/main.9ff291c4.chunk.js"
+  },
+  {
+    "revision": "30f9185ada2def07fa2f",
+    "url": "/Die-Cal/static/js/runtime-main.f8637d27.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Die-Cal/static/media/logo.ee7cd8ed.svg"
+  }
+]);
